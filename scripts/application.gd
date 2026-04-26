989f2268007882ce31a91c67f130519085b3b28a
Hromistoye <hromistaya@outlook.com>
2026-04-26 13:42:30 +0800

    限制应用交互范围在屏幕上

--- scripts/application.gd
+++ scripts/application.gd
@@ -8,15 +8,19 @@ func _ready() -> void:
 	icon_app_node.mouse_filter=Control.MOUSE_FILTER_STOP
 	pass 
 func _process(delta: float) -> void:
+	window_app_node.position.x=clamp(window_app_node.position.x,Env.SCREEN_LEFT,Env.SCREEN_RIGHT-window_app_node.size.x)
+	window_app_node.position.y=clamp(window_app_node.position.y,Env.SCREEN_TOP+Env.WINDOW_TITLE_BAR_WIDTH,Env.SCREEN_BOTTOM-window_app_node.size.y)
 	if dragging:
 		global_position=get_global_mouse_position()-drag_offset
+		global_position.x=clamp(global_position.x,Env.SCREEN_LEFT,Env.SCREEN_RIGHT-Env.ICON_SIZE)
+		global_position.y=clamp(global_position.y,Env.SCREEN_TOP,Env.SCREEN_BOTTOM-Env.ICON_SIZE)
 	pass
 func _on_icon_app_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index==MOUSE_BUTTON_LEFT and event.double_click:
 			dragging=false
-			window_app_node.size=Vector2(288,167)
-			window_app_node.position=Vector2(64,57)
+			window_app_node.size=Vector2(Env.SCREEN_RIGHT-Env.SCREEN_LEFT,Env.SCREEN_BOTTOM-Env.SCREEN_TOP+Env.WINDOW_TITLE_BAR_WIDTH)
+			window_app_node.position=Vector2(Env.SCREEN_LEFT,Env.SCREEN_TOP+Env.WINDOW_TITLE_BAR_WIDTH)
 			window_app_node.show()
 			icon_app_node.mouse_filter=Control.MOUSE_FILTER_IGNORE
 			get_viewport().set_input_as_handled()
--- scripts/env.gd
+++ scripts/env.gd
@@ -3,7 +3,8 @@ const SCREEN_TOP=16*2
 const SCREEN_BOTTOM=112*2
 const SCREEN_LEFT=32*2
 const SCREEN_RIGHT=176*2
-
+const ICON_SIZE=16*2
+const WINDOW_TITLE_BAR_WIDTH=25
 func _ready() -> void:
 	pass # Replace with function body.
 
